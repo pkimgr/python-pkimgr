63b60ef79445df8ed8a0120764101b4c86c152ea
Wampixel <>
2022-07-22 20:46:02 +0000

    Bump version: 1.2.0 → 1.3.0

--- src/pkimgrlib/__init__.py
+++ src/pkimgrlib/__init__.py
@@ -6,7 +6,7 @@ Maintainer: Wampixel
 Contact: Issue on https://gitlab.com/pkimgr/python-pkimgr
 """
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 # Certs part
 from pkimgrlib.pki.pki import Pki
